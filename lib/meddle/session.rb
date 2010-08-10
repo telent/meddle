4f8ffd933417555cec4c651ad58d04ba4e371b0b
Daniel Barlow <dan@telent.net>
2010-08-10 17:09:46 +0100

    Meddle::Session reintroduce per-tx filter block

--- lib/meddle/session.rb
+++ lib/meddle/session.rb
@@ -1,6 +1,5 @@
 
 class Meddle::Session 
-  attr_reader :start_time_offset
   include Enumerable
   def each(&blck)
     @transactions.each(&blck)
@@ -8,12 +7,20 @@ class Meddle::Session
   def [](index)
     @transactions[index]
   end
-  def initialize(file,options)
+
+  # new accepts a block which is called for each transaction. It may
+  # change the request (headers, body) as it wishes and return the
+  # modified version, or may return a falsey value to discard this
+  # transaction from the session.  
+
+  # Discarding transactions may be useful for example if the session
+  # includes requests to hosts other than the hosts under test (e.g.
+  # google analytics)
+  def initialize(file)
     doc=File.open(file) do |f|
       Nokogiri::XML(f)
     end
-    @transactions=doc.root.css('tdRequest').map {|x| Transaction.from_xml(x)}
-    @orig_start_time=@transactions[0].start_time
-    @start_time_offset=options[:delay] || 0
+    txs=doc.root.css('tdRequest').map {|x| Transaction.from_xml(x)}
+    @transactions=txs.map { |tx| (yield tx) || nil }.reject(&:nil?)
   end
 end
