a861e9c112a3dc621090ac14c28abbbcec804c19
Daniel Barlow <dan@telent.net>
2010-08-11 11:31:22 +0100

    a queue pushes from the other end to where it pops, duh

--- lib/meddle/queue.rb
+++ lib/meddle/queue.rb
@@ -1,7 +1,17 @@
 
-class Meddle::Queue < Array
+class Meddle::Queue 
+  attr_reader :data
+  def initialize
+    @data=[]
+  end
   def peek
-    self[0]
+    @data[0]
+  end
+  def pop
+    @data.shift or raise "pop on empty queue"
+  end
+  def push(v)
+    @data.push(v)
   end
 end
 
