9aaaa72fb2db002353cbbd32a0a2c117d31e1fd6
Daniel Barlow <dan@telent.net>
2010-08-13 15:23:55 +0100

    clean up the example a bit and make it profile itself with appropriate  command line arg

--- example.rb
+++ example.rb
@@ -1,5 +1,13 @@
 require 'meddle'
-require 'ruby-prof'
+require 'trollop'
+
+opts=Trollop::options do
+  opt :profile, "Profile the run using ruby-prof"
+  opt :profile_graph, "Profile output file", :default=>"callgraph.prof"
+  opt :dry_run, "Show the script, don't run it", :short=>'-n'
+end
+
+opts[:profile] and require 'ruby-prof'
 
 class BasketPurchase < Meddle::Session
   script_file "rq-registered-success.xml" do |tx|
@@ -36,17 +44,47 @@ class BasketPurchase < Meddle::Session
   end
 end
 
-EM.run do
-  meddle=Meddle::Agent.new do |agent|
-    1.times do |i| 
-      p=BasketPurchase.new
-      agent.add_session p
-      #agent.add_session BasketPurchase.new,2
-      #agent.add_session BasketPurchase.new,5
-    end
+if opts[:dry_run] then
+  b=BasketPurchase.new
+  b.each do |tx|
+    print "#{tx.request.method} #{tx.request.uri.to_s} #{tx.request.body}\n"
   end
-  warn "running until #{meddle.finish_time}"
-  EM.add_timer(meddle.finish_time-Time.now+5) do
-    EM.stop
+  exit 0
+end
+
+begin
+  EM.run do
+    meddle=Meddle::Agent.new do |agent|
+      10.times do |i| 
+        agent.add_session BasketPurchase.new
+        agent.add_session BasketPurchase.new,2
+        agent.add_session BasketPurchase.new,5
+        agent.add_session BasketPurchase.new,7
+        agent.add_session BasketPurchase.new,15
+        agent.add_session BasketPurchase.new,20
+        agent.add_session BasketPurchase.new,30
+        agent.add_session BasketPurchase.new,40
+        agent.add_session BasketPurchase.new,51
+        agent.add_session BasketPurchase.new,65
+      end
+    end
+    warn "running until #{meddle.finish_time}"
+    EM.add_timer(meddle.finish_time-Time.now+5) do
+      EM.stop
+    end
+    opts[:profile] && RubyProf.start
+  end
+rescue Exception => e
+  raise e
+ensure 
+  if opts[:profile] then
+    result = RubyProf.stop
+    
+    printer = RubyProf::GraphPrinter.new(result)
+    File.open(opts[:profile_graph],"w") do |f|
+      printer.print(f, {})
+    end
+    printer = RubyProf::FlatPrinter.new(result)
+    printer.print(STDOUT, {})
   end
 end
